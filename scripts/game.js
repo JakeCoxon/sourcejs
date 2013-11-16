5123ab6aca69a804908809d513fbdd4489731480
Jake Coxon <jake.coxon@gmail.com>
2013-11-16 12:23:27 +0000

    Added better drawing methods and drag handler

--- scripts/game.js
+++ scripts/game.js
@@ -44,6 +44,10 @@ define(function(require, exports, module) {
 
     this.setFilled(this.startX, this.startY, true);
 
+    this.dragger = new RectDragger(T * this.width, T * this.height, 0, 0, 32);
+
+    this.spriteCache = {};
+
 
     window.game = this;
   }
@@ -90,10 +94,11 @@ define(function(require, exports, module) {
 
     var sprites = this.sprites;
 
-    ctx.save();
     ctx.setTransform(1, 0, 0, 1, 0, 0);
     ctx.clearRect(0, 0, this.windowWidth, this.windowHeight);
-    ctx.restore();
+    
+
+    ctx.setTransform(1, 0, 0, 1, this.dragger.x, this.dragger.y);
 
     // for (var i = 0; i < 10; i++) {
     //   for (var j = 0; j < 10; j++) {
@@ -106,29 +111,60 @@ define(function(require, exports, module) {
 
     for (i = 0; i < this.background.cols; i++) {
       for (j = 0; j < this.background.rows; j++) {
-        ctx.drawImage(sprites, 5 * 64, 0, 64, 64, (this.background.x + T * i) * this.scale, (this.background.y + T * j) * this.scale, T * this.scale, T * this.scale);
+        this.drawSprite(ctx, 5, 0, this.background.x + T * i, this.background.y + T * j, 0);
       }
     }
+
+    // for (i = 0; i < this.background.cols; i++) {
+    //   for (j = 0; j < this.background.rows; j++) {
+    //     ctx.drawImage(sprites, 5 * 64, 0, 64, 64, (this.background.x + T * i) * this.scale, (this.background.y + T * j) * this.scale, T * this.scale, T * this.scale);
+    //   }
+    // }
+
+    ctx.save();
+    ctx.translate(this.boardX, this.boardY);
 
     for (i = 0; i < this.width; i++) {
       for (j = 0; j < this.height; j++) {
 
-        ctx.save();
         var tile = this.tiles[i + j * this.width];
-        ctx.translate((T * i + T/2 + this.boardX) * this.scale, (T * j + T/2 + this.boardY) * this.scale);
-        ctx.rotate(tile.rotation * Math.PI / 2);
-        ctx.translate((-T * i - T/2 - this.boardX) * this.scale, (-T * j - T/2 - this.boardY) * this.scale);
 
         var sprite = tile.type, on = this.filled[i + j * this.width] ? 1 : 0;
         if (i == this.startX && j == this.startY) {
           sprite = 5; on = 1;
         }
-        ctx.drawImage(sprites, sprite * 64, on * 64, 64, 64, (T * i + this.boardX) * this.scale, (T * j + this.boardY) * this.scale, T * this.scale, T * this.scale);
 
-        ctx.restore();
+        this.drawSprite(ctx, sprite, on, T * i, T * j, tile.rotation);
+
       }
     }
+    ctx.restore();
+
   };
+
+  Game.prototype.rotateImage = function(image, sx, sy, w, h, angle) {
+    var offscreenCanvas = document.createElement('canvas');
+    var offscreenCtx = offscreenCanvas.getContext('2d');
+
+    var size = Math.max(w, h);
+    offscreenCanvas.width = size;
+    offscreenCanvas.height = size;
+
+    offscreenCtx.translate(size/2, size/2);
+    offscreenCtx.rotate(angle * Math.PI / 2);
+    offscreenCtx.drawImage(image, sx, sy, w, h, -w/2, -h/2, w, h);
+
+    return offscreenCanvas;
+  };
+
+  Game.prototype.drawSprite = function(ctx, tx, ty, x, y, angle) {
+    
+    var c = "" + tx + ty + angle;
+    if (!this.spriteCache[c])
+      this.spriteCache[c] = this.rotateImage(this.sprites, tx * 64, ty * 64, 64, 64, angle);
+
+    ctx.drawImage(this.spriteCache[c], x, y);
+  }
 
   Game.prototype.resize = function(width, height) {
     this.windowWidth = width;
@@ -136,72 +172,93 @@ define(function(require, exports, module) {
     this.boardX = (width - this.width * T) / 2;
     this.boardY = (height - this.height * T) / 2;
     this.background.setSize(width, height);
+    this.dragger.containerWidth = width;
+    this.dragger.containerHeight = height;
+    this.dragger.contain();
   };
-
-  var drag = {x: 0, y: 0};
-  var offsetX = 0, offsetY = 0;
-  var mouseX = 0, mouseY = 0;
-  var boardX = 0, boardY = 0;
-  var tboardX = 0, tboardY = 0;
-  var click = true;
 
   Game.prototype.touchstart = function(e) {
-    drag.x = e.touches[0].clientX;
-    drag.y = e.touches[0].clientY;
-    mouseX = e.touches[0].clientX;
-    mouseY = e.touches[0].clientY;
-    click = true;
+    this.dragger.press(e.touches[0].clientX, e.touches[0].clientY);
+    this.click = true;
   };
   Game.prototype.touchmove = function(e) {
-    offsetX = e.touches[0].clientX - drag.x;
-    offsetY = e.touches[0].clientY - drag.y;
-
-
-    tboardX = boardX + offsetX;
-    tboardY = boardY + offsetY;
+    this.dragger.drag(e.touches[0].clientX, e.touches[0].clientY);
 
     // tboardX = Math.min(Math.max(boardX + offsetX, 568 - this.width * T), 0);
     // tboardY = Math.min(Math.max(boardY + offsetY, 320 - this.height * T), 0);
     // var s = (100 + offsetX) / 100;
     // this.scale = s;
-    this.ctx.setTransform(1, 0, 0, 1, tboardX, tboardY);
-    mouseX = e.touches[0].clientX;
-    mouseY = e.touches[0].clientY;
 
-    if (Math.abs(offsetX) > 10 || Math.abs(offsetY) > 10)
-      click = false;
+    if (Math.abs(this.dragger.mouseDistX) > 10 || Math.abs(this.dragger.mouseDistY) > 10)
+      this.click = false;
     
   };
   Game.prototype.touchend = function(e) {
-    boardX = tboardX;
-    boardY = tboardY;
 
-    if (!click) return;
+    if (!this.click) return;
 
-    var x = mouseX / this.scale - (this.boardX) - boardX / this.scale,
-        y = mouseY / this.scale - (this.boardY) - boardY / this.scale;
+    // var mouseX = e.touches[0].clientX,
+    //     mouseY = e.touches[0].clientY;
+
+    // var x = mouseX / this.scale - (this.boardX) - boardX / this.scale,
+    //     y = mouseY / this.scale - (this.boardY) - boardY / this.scale;
 
 
-    var xTile = Math.floor(x / T),
-        yTile = Math.floor(y / T);
+    // var xTile = Math.floor(x / T),
+    //     yTile = Math.floor(y / T);
 
-    this.tiles[xTile + yTile * 10].rotate();
-    this.resetAll();
-    this.setFilled(this.startX, this.startY, true);
+    // this.tiles[xTile + yTile * 10].rotate();
+    // this.resetAll();
+    // this.setFilled(this.startX, this.startY, true);
   };
 
   Game.prototype.scroll = function(delta) {
     this.scale += delta / 20;
   };
 
-  function Background() {
-
-  }
+  function Background() { }
   Background.prototype.setSize = function(width, height) {
+    width += 64; height += 64;
     this.cols = Math.floor(width / T / 2) * 2 + 2;
     this.rows = Math.floor(height / T / 2) * 2 + 2;
     this.x = (width - this.cols * T) / 2;
     this.y = (height - this.rows * T) / 2;
+  };
+
+  function Drag() {
+    this.mouseDownX = 0; this.mouseDownY = 0;
+    this.x = 0; this.y = 0;
+    this.startX = 0; this.startY = 0;
+  }
+  Drag.prototype.press = function(x, y) {
+    this.mouseDownX = x; this.mouseDownY = y;
+    this.startX = this.x; this.startY = this.y;
+  };
+  Drag.prototype.drag = function(x, y) {
+    this.mouseDistX = x - this.mouseDownX;
+    this.mouseDistY = y - this.mouseDownY;
+    this.x = this.startX + this.mouseDistX;
+    this.y = this.startY + this.mouseDistY;
+  };
+
+  function RectDragger(iw, ih, cw, ch, margin) {
+    this.innerWidth = iw; this.innerHeight = ih;
+    this.containerWidth = cw; this.containerHeight = ch;
+    this.margin = margin;
+  }
+
+  RectDragger.prototype = new Drag();
+
+  RectDragger.prototype.drag = function(x, y) {
+    Drag.prototype.drag.call(this, x, y);
+    this.contain();
+  }
+  RectDragger.prototype.contain = function() {
+    var top = Math.max(0, (this.innerHeight - this.containerHeight)/2 + this.margin);
+    var left = Math.max(0, (this.innerWidth - this.containerWidth)/2 + this.margin);
+
+    this.x = Math.min(Math.max(this.x, -left), left);
+    this.y = Math.min(Math.max(this.y, -top), top);
   };
 
 
